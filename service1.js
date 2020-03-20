d76e6fd127b0b4fc992d7bb1288f0bae1df3d0af
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-03-19 21:41:10 -0700

    coursework

--- service1.js
+++ service1.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const redis = require('redis');
+const app = express();
+const client = redis.createClient();
 const port = 3001;
 
-let counter = 0;
+let counter = 0; // instance variable, bad, don't do this
 
 app.get('/service1/*', (req, res) => {
-  counter++;
-  console.log('Hitting service', process.env.NODE_APP_INSTANCE);
-  res.send(`Hello from instance: ${process.env.NODE_APP_INSTANCE}, ${counter} Visits!!!`);
+  // counter++;
+  // Alert others when this endpoint has been triggered
+  client.publish('myPubSubChannel', `${process.env.NODE_APP_INSTANCE} has been visited.`);
+  client.incr('myCounter', (err, updatedValue) => {
+    // redis takes care of sync and returns new value
+    if (err) console.log(err); // only if broken
+    // this is after redis responds
+    console.log('Hitting service', process.env.NODE_APP_INSTANCE); // which clone is it?
+    res.send(`Hello from instance: ${process.env.NODE_APP_INSTANCE}, ${updatedValue} Visits!!!`);
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- service2.js
+++ service2.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const redis = require('redis');
 const app = express();
+const client = redis.createClient();
 const port = 3002;
 
-app.get('/service2/*', (req, res) => res.send('Service 2'));
+client.on('message', (channel, message) => {
+  // messages coming from service 1 cluster publish via pub sub
+  console.log(`${channel}: ${message}`);
+});
+client.subscribe('myPubSubChannel');
+
+app.get('/service2/set', (req, res) => {
+  client.set('myOtherValue', req.query.value || 'backup', (err, value) => {
+    res.send('Your value has been set');
+  });
+});
+
+app.get('/service2/get', (req, res) => {
+  client.get('myOtherValue', (err, value) => {
+    res.send(`The current value is ${value}`);
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
